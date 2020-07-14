cebbd79261cb4cb4c6df1c72a485a3c56d23188f
Jérôme Heissler <jerome@prototyper.fr>
2020-07-14 16:02:17 +0200

    add mysql server

--- provisioning.sh
+++ provisioning.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+DBHOST=localhost
+DBNAME=petclinic
+DBUSER=root
+DBPASSWD=root
 
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list
 
 apt-get update
 apt-get install -y default-jdk curl openssh-server ca-certificates
-apt-get install -y jenkins maven docker.io docker-compose mysql-server
+apt-get install -y jenkins maven docker.io docker-compose
 
 usermod -a -G docker jenkins
 usermod -a -G docker vagrant
@@ -16,4 +20,17 @@ systemctl enable docker
 
 ufw allow 8080
 
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
 
+# install mysql and admin interface
+
+apt-get -y install mysql-server
+
+mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
+mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'%' identified by '$DBPASSWD'"
+# update mysql conf file to allow remote access to the db
+
+sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+
+sudo service mysql restart
